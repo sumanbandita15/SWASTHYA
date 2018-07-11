365003bc441d6ef5d8c5afb9843808aca4b79cee
Suman Bandita <sumanbandita@gmail.com>
2018-07-11 15:41:25 -0400

    redeploy

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/swasthya-backend',
+        process.env.DATABASE_URL || 'mongodb://dev:dev123@ds233581.mlab.com:33581/swasthya',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/swasthya-backend-test',
